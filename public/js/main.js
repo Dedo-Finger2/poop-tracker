1b51c6c37ddb67a2db9c7c399e86efb648c0f7e1
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-04 20:20:15 -0300

    Adds function to create the local storage database

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-const teste = 2;
+function createLocalstorageDatabase() {
+  const storagedData = localStorage.getItem("database");
+  if (storagedData) return;
+  const data = {
+    todayPoopCount: 0,
+    streak: 0,
+    lastTimePooped: undefined,
+    history: [],
+  };
+  localStorage.setItem("database", JSON.stringify(data));
+}
+
+function main() {
+  createLocalstorageDatabase();
+}
+
+main();
